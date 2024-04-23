0073089497ddb1c4098873d10a91d621e26ea72f
unknown <sujanbhushal2014@gmail.com>
2024-04-23 09:17:53 +0545

    solving array level one assignment

---
+++ Javascript/array/level1.js
@@ -0,0 +1,17 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+let arrLength = arr.length
+console.log(arrLength)
+
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+let arrReverse = arr.reverse()
+console.log(arrReverse)
+
+///Q3 join the array and console log
+// expected output 352
+let arrJoin =arr.join('')
+console.log(arrJoin)
