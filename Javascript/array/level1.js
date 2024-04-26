f63a52ea1c9c5ceb2f7a75980c97ef7525f1d7d7
unknown <sujanbhushal2014@gmail.com>
2024-04-26 08:07:31 +0545

    sum of price object insice array using reduce

--- Javascript/array/level1.js
+++ Javascript/array/level1.js
@@ -24,10 +24,40 @@ console.log(arr)
 // Q5 Check if all elements in the num  are even. Log "All even" or "Not all even" accordingly.
 //const num = [6,4,2,8,10,12]
 
+const num = [6, 4, 2, 8, 10, 12]
+let allEven = true;
+num.forEach((element) => {
+    if (element % 2 !== 0) {
+        allEven = false
+    }
+})
+if (allEven) {
+    console.log("All even");
+} else {
+    console.log("Not all even");
+}
 
 
 // Find the index of a specific element in the array. Log the index to the console. 
 //expected output: 3
 
+
 //const allNum = [3, 5, 2, 8, 5];
 //const searchElement = 2;
+
+const allNum = [3, 5, 2, 8, 5];
+const index = allNum.indexOf(2)
+console.log(index)
+
+//sum of all price using reduce()
+//Expected outpur is 4200
+const Array1 = [
+    {id: 1, productName:'Hawkins Cooker', price:3200 },
+    {id: 14, productName:'Tomato', price:500 },
+      {id: 11, productName:'Potato', price:200 },
+      {id: 15, productName:'Sunsilk', price: 300 },
+  ]
+  const totalPrice = Array1.reduce((summation,value)=>{
+      return summation+value.price
+  },0)
+  console.log(totalPrice)
