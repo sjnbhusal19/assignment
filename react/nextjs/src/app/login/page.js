81fc6d2267784deaf103af7067bb1adeea7ab566
unknown <sujanbhushal2014@gmail.com>
2024-05-02 15:14:41 +0545

    adding Navbar on component

--- react/nextjs/src/app/login/page.js
+++ react/nextjs/src/app/login/page.js
@@ -1,26 +1,22 @@
-
 import customNav from "@/components/Navbar/page";
 import {Button,Input} from "@nextui-org/react";
 
-
-const register = () => {
+const Login = () => {
   return (
     <div>
     <customNav/>
     <div className="flex justify-center">
     <div className='border border-black m-5 w-72 p-10'>
-      <h1>Register page</h1><br/>
-      <Input type="email" label="Enter Email" /><br/>
-      <Input type="Password" label="Enter Password" /><br/>
-    <Input type="Address" label="Enter Address" /><br/>
-    <Input type="User Name" label="Enter User Name" /><br/>
+      <h1>Login page</h1><br/>
+      <Input type="email" label="Email" /><br/>
+    <input placeholder='Enter password' type='password'></input><br/>
     <Button radius="full" className="bg-gradient-to-tr from-green-500 to-yellow-500 text-white shadow-lg">
       Submit
     </Button>
     </div>
     </div>
     </div>
-  )
-}
+  );
+};
 
-export default register
+export default Login;
