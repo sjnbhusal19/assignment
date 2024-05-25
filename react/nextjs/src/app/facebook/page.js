f0968a93fa350b69a00c53e1ce5bfa4ea10e05f3
unknown <sujanbhushal2014@gmail.com>
2024-05-25 14:29:31 +0545

    adding form validation

--- react/nextjs/src/app/facebook/page.js
+++ react/nextjs/src/app/facebook/page.js
@@ -1,20 +1,63 @@
 'use client'
 import React, { useState } from 'react'
 import { GrLike } from "react-icons/gr";
+import { FaHeart, FaLaughSquint,FaRegAngry } from "react-icons/fa";
 
-const page = () => {
+
+
+
+const facebook = () => {
     const [color,setColor]= useState ('gray')
-    const handleColorChange=()=>{
-        setColor('blue')
+    const [reaction,setReaction]=useState('love')
+    const [reactionDivOpen,setReactionDivOpen]=useState(false) 
+
+    const ReactionButton=()=>{
+      return(
+        <div className='w-36 bg-gray-200 shadow-lg rounded-2xl shadow-xl'>
+        <button onMouseLeave ={() => handleColorChange()}className=' p-2 '> <GrLike color='blue'/>
+        </button>
+        <button onMouseLeave ={() => handleColorChange()}className=' p-2 '> <FaHeart color='crimson'/>
+       </button>
+       <button onMouseLeave ={() => handleColorChange()}className=' p-2 '> <FaLaughSquint color='orange'/>
+       </button>
+       <button onMouseLeave ={() => handleColorChange()}className=' p-2 '> <FaRegAngry color='red'/>
+       </button>
+        </div>
+      )
     }
+    const generateReactionButton =()=>{
+      if (reaction === 'like') {
+        return (
+          <button onMouseEnter ={() => setReactionDivOpen(true)}className='bg-gray-200 p-2 rounded-md shadow-lg '> <GrLike color={color}/>
+         </button>
+        )
+      }else if (reaction === 'love'){
+        return (
+        <button onMouseEnter ={() => setReactionDivOpen(true)}className='bg-gray-200 p-2 rounded-md shadow-lg '> <GrLike color={color}/>
+        </button>
+        )
+      }else if (reaction=== 'haha'){
+        return (
+          <button onMouseEnter ={() => setReactionDivOpen(true)}className='bg-gray-200 p-2 rounded-md shadow-lg '> <GrLike color={color}/>
+          </button> 
+        )
+      }else if (reaction === 'angry'){
+        return (
+          <button onMouseEnter ={() => setReactionDivOpen(true)}className='bg-gray-200 p-2 rounded-md shadow-lg '> <GrLike color={color}/>
+        </button>
+        )
+      }
+      }
+
   return (
     <div>
         Like<br/>
-       <button onClick ={() => handleColorChange()}className='bg-gray-200 p-2 border border-black'> <GrLike color={color}/>
-       </button>
-
+        <div>
+        {reactionDivOpen ? <ReactionButton/> : null}
+       {generateReactionButton()}
+       </div>
         </div>
   )
 }
 
-export default page
+export default facebook;
--- react/nextjs/src/app/form-validation/page.js
+++ react/nextjs/src/app/form-validation/page.js
@@ -1,7 +1,21 @@
+'use client'
 import React from 'react';
 import { useFormik } from 'formik';
-
-
+import {Input} from "@nextui-org/react";
+import * as Yup from 'yup';
+ 
+ const SignupSchema = Yup.object().shape({
+   firstName: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('Required'),
+   lastName: Yup.string()
+     .min(2, 'Too Short!')
+     .max(50, 'Too Long!')
+     .required('Required'),
+   email: Yup.string().email('Invalid email').required('Required'),
+ });
+ 
 
 const SignupForm = () => {
   const formik = useFormik({
@@ -10,6 +24,7 @@ const SignupForm = () => {
       lastName: '',
       email: '',
     },
+    validationSchema:SignupSchema,
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -17,29 +32,30 @@ const SignupForm = () => {
   return (
     <form onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
+      <Input className='w-[25%]'
         name="firstName"
-        type="text"
+        placeholder='Enter your first name'
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
+      {formik.errors.firstName}
       <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
+      <Input className='w-[25%]'
         name="lastName"
         type="text"
         onChange={formik.handleChange}
         value={formik.values.lastName}
       />
+            {formik.errors.lastName}
       <label htmlFor="email">Email Address</label>
-      <input
+      <Input className='w-[25%]'
         id="email"
         name="email"
         type="email"
         onChange={formik.handleChange}
         value={formik.values.email}
       />
+      {formik.errors.email}
       <button type="submit">Submit</button>
     </form>
   );
